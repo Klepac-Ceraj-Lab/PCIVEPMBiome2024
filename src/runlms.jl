9ba27c3d538a546985a071200edd2d788a5794e2
Guilherme Fabur Bottino <gfahurbottino@gmail.com>
2026-05-07 02:51:59 -0400

    added block to compute and expor Cook D on runlms.jl

--- src/runlms.jl
+++ src/runlms.jl
@@ -1,27 +1,49 @@
 function runlms(
     indf::DataFrame, feature_vector::Vector{String},# outfile::String,
-    modcols=[ "mbiome_sample_age", "child_sex", "InfantVisAtt", "MaternalEntropy" ];
+    modcols=[ "mbiome_sample_age", "child_sex", "maternal_education", "feeding_state", "delivery_mode", "InfantVisAtt", "MaternalEntropy" ];
     prevalence_threshold = 0.1,
     abundance_threshold = 1.0,
     model_kind = :linear, # OR :logistic
     bug_preproc = :none, # [:none, :autoscale, :log2, :arcsin]
-    bugrank = false
+    bugrank = false,
+    sample_id_col::Symbol = :sample,
+    outdir::String = joinpath("manuscript", "figures", "glm"),
+    export_cooks::Bool = true
     )
 
-    f1=@formula(bug ~ mbiome_sample_age + child_sex + MaternalEntropy)
-    f2=@formula(bug ~ mbiome_sample_age + child_sex + InfantVisAtt)
-    f3=@formula(bug ~ mbiome_sample_age + child_sex + MaternalEntropy + InfantVisAtt + InfantVisAtt*MaternalEntropy)
+    isdir(outdir) && throw(error("Output directory already exists!"))
+    mkpath(outdir)
+
+    ## Accessory function for cooks D
+    function cooks_distance_lm(this_model)
+        X = modelmatrix(this_model)
+        r = residuals(this_model)
+
+        n, p = size(X)
+        σ2 = sum(r .^ 2) / (n - p)
+
+        H = X * inv(X' * X) * X'
+        h = LinearAlgebra.diag(H)
+
+        D = (r .^ 2 ./ (p * σ2)) .* (h ./ (1 .- h).^2)
+
+        return D
+    end
+
+    f1=@formula(bug ~ mbiome_sample_age + child_sex + maternal_education + feeding_state + delivery_mode + MaternalEntropy)
+    f2=@formula(bug ~ mbiome_sample_age + child_sex + maternal_education + feeding_state + delivery_mode + InfantVisAtt)
+    f3=@formula(bug ~ mbiome_sample_age + child_sex + maternal_education + feeding_state + delivery_mode + MaternalEntropy + InfantVisAtt + InfantVisAtt*MaternalEntropy)
 
     # lmresults = DataFrame(ThreadsX.map(feature_vector) do this_feature
     lmresults = DataFrame(map(feature_vector) do this_feature
 
     @debug this_feature
 
-        default_nt_f1 = (; Name = "MaternalEntropy", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, kind="f1_"*string(model_kind)*"_unirefs")
-        default_nt_f2 = (; Name = "InfantVisAtt", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, kind="f2_"*string(model_kind)*"_unirefs")
-        default_nt_f3_MatEnt = (; Name = "MaternalEntropy", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, kind="f3_"*string(model_kind)*"_unirefs")
-        default_nt_f3_InfVis = (; Name = "InfantVisAtt", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, kind="f3_"*string(model_kind)*"_unirefs")
-        default_nt_f3_Interact = (; Name = "InfantVisAtt & MaternalEntropy", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, kind="f3_"*string(model_kind)*"_unirefs")
+        default_nt_f1 = (; Name = "MaternalEntropy", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, rsquare=NaN, kind="f1_"*string(model_kind)*"_taxa")
+        default_nt_f2 = (; Name = "InfantVisAtt", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, rsquare=NaN, kind="f2_"*string(model_kind)*"_taxa")
+        default_nt_f3_MatEnt = (; Name = "MaternalEntropy", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, rsquare=NaN, kind="f3_"*string(model_kind)*"_taxa")
+        default_nt_f3_InfVis = (; Name = "InfantVisAtt", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, upper_95=NaN, rsquare=NaN, kind="f3_"*string(model_kind)*"_taxa")
+        default_nt_f3_Interact = (; Name = "InfantVisAtt & MaternalEntropy", feature = this_feature, coef = NaN, std_err = NaN, stat = NaN, pvalue = NaN, lower_95=NaN, rsquare=NaN, upper_95=NaN, kind="f3_"*string(model_kind)*"_taxa")
 
         ab = vec(indf[:, this_feature])
         over0 = ab .> 0
@@ -32,10 +54,10 @@ function runlms(
         elseif ( mean(ab[over0]) < abundance_threshold )
             return(
                 default_nt_f1, default_nt_f2, default_nt_f3_MatEnt, default_nt_f3_InfVis, default_nt_f3_Interact
-            )
+            )       
         end
 
-        df = indf[!, modcols]
+        df = select(indf, vcat([sample_id_col], Symbol.(modcols)))
 
         if model_kind == :linear
 
@@ -43,6 +65,12 @@ function runlms(
 
             df.bug = ab
             df = subset(df, :bug => (x -> (x .> 0.0)))
+
+            if ( ( !all(map( x -> length(unique(x)), eachcol(select(df, modcols))) .> 1) ) | ( nrow(df) <= length(modcols) ) )
+                return(
+                    default_nt_f1, default_nt_f2, default_nt_f3_MatEnt, default_nt_f3_InfVis, default_nt_f3_Interact
+                )
+            end
 
             if bugrank == true
 
@@ -83,6 +111,26 @@ function runlms(
             lmodf2 = lm(f2, df; dropcollinear=true)
             lmodf3 = lm(f3, df; dropcollinear=true)
 
+            if export_cooks
+            
+                try
+                    cooks_df = DataFrame(
+                        sample_id = df[!, sample_id_col],
+                        feature = this_feature,
+                        cooksD_f1 = cooks_distance_lm(lmodf1),
+                        cooksD_f2 = cooks_distance_lm(lmodf2),
+                        cooksD_f3 = cooks_distance_lm(lmodf3)
+                    )
+
+                    CSV.write(
+                        joinpath(outdir, "CooksD_$(this_feature).csv"),
+                        cooks_df
+                    )
+                catch e
+                end
+            
+            end
+
             ## Figure generation step
             fig = Figure()
             ax = Axis(
@@ -94,7 +142,7 @@ function runlms(
             # Plot the data points
             scatter!(ax, df.InfantVisAtt, df.bug, color = (:blue, 0.6), markersize=8)
             save(
-                joinpath("manuscript", "figures", "glm", "Linear_$(this_feature).png"),
+                joinpath(outdir, "Linear_$(this_feature).png"),
                 fig
             )    
 
@@ -142,96 +190,52 @@ function runlms(
             # Optionally, overlay the original data points
             scatter!(ax, df.InfantVisAtt, df.bug, color=:blue, markersize=8)
             save(
-                joinpath("manuscript", "figures", "glm", "Logistic_$(this_feature).png"),
+                joinpath(outdir, "Logistic_$(this_feature).png"),
                 fig
             )         
 
         else
-
-            throw(ArgumentError("No model_kind: $model_kind"))    
-
+            throw(ArgumentError("No model_kind: $model_kind"))
         end
 
-        # # ————————————————
-        # # helper: median p-value by subsampling
-        # function bootstrap_median_p(formula, data; row_idx::Int, B=100, frac=0.8)
-        #     n = nrow(data)
-        #     ps = Float64[]
-        #     for _ in 1:B
-        #         idx = sample(1:n, round(Int,frac*n), replace=false)
-        #         sub = data[idx, :]
-        #         m = glm(formula, sub, GLM.Gamma(), LogLink(); maxiter=1000)
-        #         ct = DataFrame(coeftable(m))
-        #         push!(ps, ct[row_idx, :"Pr(>|z|)"])
-        #     end
-        #     return median(ps)
-        # end
-        # # ————————————————
-        # # bootstrap and overwrite p-values in each fit
-        # # f1’s coeff “MaternalEntropy” is row 4
-        # p1_med = bootstrap_median_p(f1, df; row_idx=4, B=200, frac=0.5)
-        # df1 = DataFrame(coeftable(lmodf1))
-        # df1[4, :"Pr(>|z|)"] = p1_med
-
-        # # f2’s coeff “InfantVisAtt” is row 4
-        # p2_med = bootstrap_median_p(f2, df; row_idx=4, B=200, frac=0.5)
-        # df2 = DataFrame(coeftable(lmodf2))
-        # df2[4, :"Pr(>|z|)"] = p2_med
-
-        # # f3: MaternalEntropy row 4, InfantVisAtt row 5, Interaction row 6
-        # p3_m = bootstrap_median_p(f3, df; row_idx=4, B=200, frac=0.5)
-        # p3_i = bootstrap_median_p(f3, df; row_idx=5, B=200, frac=0.5)
-        # p3_x = bootstrap_median_p(f3, df; row_idx=6, B=200, frac=0.5)
-        # df3 = DataFrame(coeftable(lmodf3))
-        # df3[4, :"Pr(>|z|)"] = p3_m
-        # df3[5, :"Pr(>|z|)"] = p3_i
-        # df3[6, :"Pr(>|z|)"] = p3_x
-        # # ————————————————
-
-        # # the rest of your postformat logic, but now using df1, df2, df3
-        # ct1 = df1[4:4, :]
-        # ct2 = df2[4:4, :]
-        # ct3 = df3[4:4, :]
-        # ct4 = df3[5:5, :]
-        # ct5 = df3[6:6, :]
-
-        ct1 = DataFrame(coeftable(lmodf1))[4:4, :]
+        ct1 = DataFrame(coeftable(lmodf1))[7:7, :]
         @assert ct1.Name[1] == "MaternalEntropy"
 
-        ct2 = DataFrame(coeftable(lmodf2))[4:4, :]
+        ct2 = DataFrame(coeftable(lmodf2))[7:7, :]
         @assert ct2.Name[1] == "InfantVisAtt"
 
-        ct3 = DataFrame(coeftable(lmodf3))[4:4, :]
+        ct3 = DataFrame(coeftable(lmodf3))[7:7, :]
         @assert ct3.Name[1] == "MaternalEntropy"
 
-        ct4 = DataFrame(coeftable(lmodf3))[5:5, :]
+        ct4 = DataFrame(coeftable(lmodf3))[8:8, :]
         @assert ct4.Name[1] == "InfantVisAtt"
 
-        ct5 = DataFrame(coeftable(lmodf3))[6:6, :]
+        ct5 = DataFrame(coeftable(lmodf3))[9:9, :]
         @assert ct5.Name[1] == "InfantVisAtt & MaternalEntropy"
 
-        function postformat(ct, featnmm)
+        function postformat(ct, featnmm, this_model)
             ct.feature = [ featnmm ]
             rename!(ct, "Lower 95%"=> "lower_95", "Upper 95%"=> "upper_95", "Coef."=> "coef", "Std. Error"=>"std_err")
             model_kind == :linear && rename!(ct, "Pr(>|t|)"=>"pvalue", "t"=>"stat")
             model_kind == :logistic && rename!(ct, "Pr(>|z|)"=>"pvalue", "z"=>"stat")
             # rename!(ct, "Pr(>|z|)"=>"pvalue", "z"=>"stat")
+            ct.rsquare = ( (model_kind == :linear) ? [r2(this_model)] : [NaN] )
             select!(ct, Cols(:Name, :feature, :))
             return(ct[1, :])
         end
 
-        ct1_fmt = postformat(ct1, this_feature)
-        ct2_fmt = postformat(ct2, this_feature)
-        ct3_fmt = postformat(ct3, this_feature)
-        ct4_fmt = postformat(ct4, this_feature)
-        ct5_fmt = postformat(ct5, this_feature)
+        ct1_fmt = postformat(ct1, this_feature, lmodf1)
+        ct2_fmt = postformat(ct2, this_feature, lmodf2)
+        ct3_fmt = postformat(ct3, this_feature, lmodf3)
+        ct4_fmt = postformat(ct4, this_feature, lmodf3)
+        ct5_fmt = postformat(ct5, this_feature, lmodf3)
 
         return (
-            (; ct1_fmt... , kind="f1_"*string(model_kind)*"_unirefs"),
-            (; ct2_fmt... , kind="f2_"*string(model_kind)*"_unirefs"),
-            (; ct3_fmt... , kind="f3_"*string(model_kind)*"_unirefs"),
-            (; ct4_fmt... , kind="f3_"*string(model_kind)*"_unirefs"),
-            (; ct5_fmt... , kind="f3_"*string(model_kind)*"_unirefs")
+            (; ct1_fmt... , kind="f1_"*string(model_kind)*"_taxa"),
+            (; ct2_fmt... , kind="f2_"*string(model_kind)*"_taxa"),
+            (; ct3_fmt... , kind="f3_"*string(model_kind)*"_taxa"),
+            (; ct4_fmt... , kind="f3_"*string(model_kind)*"_taxa"),
+            (; ct5_fmt... , kind="f3_"*string(model_kind)*"_taxa")
         )
 
         end)
