10740a770175dcbc90844f186e840ea95600c5a2
Guilherme Fabur Bottino <gfahurbottino@gmail.com>
2026-05-07 02:53:00 -0400

    edited paths for inputs and outputs of FSEA

--- notebooks/fsea_analysis.jl
+++ notebooks/fsea_analysis.jl
@@ -3,27 +3,43 @@
 #####
 
 ## Model 1
-perform_fsea( "manuscript/FSEA/lms_Model1_child_sex.csv", "manuscript/FSEA/fsea_consolidated_Model1_child_sex.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model1_mbiome_age.csv", "manuscript/FSEA/fsea_consolidated_Model1_mbiome_age.csv"; should_consolidate=true)
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model1_child_sex.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model1_child_sex.csv");
+    should_consolidate=true
+)
+
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model1_mbiome_age.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model1_mbiome_age.csv");
+    should_consolidate=true
+)
 
 ## Model 2
-perform_fsea( "manuscript/FSEA/lms_Model2_child_sex.csv", "manuscript/FSEA/fsea_consolidated_Model2_child_sex.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model2_mbiome_age.csv", "manuscript/FSEA/fsea_consolidated_Model2_mbiome_age.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model2_visual.csv", "manuscript/FSEA/fsea_consolidated_Model2_visual.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model2_entropy.csv", "manuscript/FSEA/fsea_consolidated_Model2_entropy.csv"; should_consolidate=true)
-
-## Model 2.1 (Entropy)
-perform_fsea( "manuscript/FSEA/lms_Model21_child_sex.csv", "manuscript/FSEA/fsea_consolidated_Model21_child_sex.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model21_mbiome_age.csv", "manuscript/FSEA/fsea_consolidated_Model21_mbiome_age.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model21_entropy.csv", "manuscript/FSEA/fsea_consolidated_Model21_entropy.csv"; should_consolidate=true)
-
-## Model 2.2 (Visual)
-perform_fsea( "manuscript/FSEA/lms_Model22_child_sex.csv", "manuscript/FSEA/fsea_consolidated_Model22_child_sex.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model22_mbiome_age.csv", "manuscript/FSEA/fsea_consolidated_Model22_mbiome_age.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model22_visual.csv", "manuscript/FSEA/fsea_consolidated_Model22_visual.csv"; should_consolidate=true)
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model2_visual.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model2_visual.csv");
+    should_consolidate=true
+)
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model2_entropy.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model2_entropy.csv");
+    should_consolidate=true
+)
 
 ## Model 3
-perform_fsea( "manuscript/FSEA/lms_Model3_child_sex.csv", "manuscript/FSEA/fsea_consolidated_Model3_child_sex.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model3_visual.csv", "manuscript/FSEA/fsea_consolidated_Model3_visual.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model3_entropy.csv", "manuscript/FSEA/fsea_consolidated_Model3_entropy.csv"; should_consolidate=true)
-perform_fsea( "manuscript/FSEA/lms_Model3_product.csv", "manuscript/FSEA/fsea_consolidated_Model3_product.csv"; should_consolidate=true)
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model3_visual.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model3_visual.csv");
+    should_consolidate=true
+)
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model3_entropy.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model3_entropy.csv");
+    should_consolidate=true
+)
+perform_fsea(
+    joinpath(Base.pwd(), "manuscript", "gene_glm_previousmodel", "lms_Model3_product.csv"),
+    joinpath(Base.pwd(), "manuscript", "FSEA", "fsea_consolidated_Model3_product.csv");
+    should_consolidate=true
+)
