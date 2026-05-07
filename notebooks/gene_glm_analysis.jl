f3ce4ce0a8e87c9cbe95c94cde4d99ceabee0317
Guilherme Fabur Bottino <gfahurbottino@gmail.com>
2026-05-07 02:53:47 -0400

    harmonzied, added covariables and cleaned up gene GLMs

--- notebooks/gene_glm_analysis.jl
+++ notebooks/gene_glm_analysis.jl
@@ -9,22 +9,26 @@ using ProgressMeter
 
 # Input data for all GLM models
 
-indf = DataFrame(get(unstratified_unirefs_filtered))
-# indf.InfantVisAtt = Leap.rangenormalize(indf.InfantVisAtt)
-# indf.MaternalEntropy = Leap.rangenormalize(indf.MaternalEntropy)
-# indf.mbiome_sample_age = Leap.rangenormalize(indf.mbiome_sample_age)
-indf.InfantVisAtt = Leap.autonormalize(indf.InfantVisAtt)
-indf.MaternalEntropy = Leap.autonormalize(indf.MaternalEntropy)
-indf.mbiome_sample_age = Leap.autonormalize(indf.mbiome_sample_age)
+indf = deepcopy(unimdata)
 
+indf.mbiome_sample_age = Leap.rangenormalize(indf.mbiome_sample_age)
+indf.maternal_education = Leap.rangenormalize(indf.maternal_education)
+
+indf.feeding_state = Float64.(indf.feeding_state .== "ExclBreastFed") 
+indf.delivery_mode = Float64.(indf.delivery_mode .== "Caesarean")
+
+indf.MaternalEntropy = Leap.rangenormalize(indf.MaternalEntropy)
+indf.InfantVisAtt = Leap.rangenormalize(indf.InfantVisAtt)
 ## convenience
 featlist = collect(featurenames(unstratified_unirefs_filtered))
 
 # MODEL 1
-
 outfiles1 = (
-    "manuscript/FSEA/lms_Model1_mbiome_age.csv",
-    "manuscript/FSEA/lms_Model1_child_sex.csv"
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model1_mbiome_age.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model1_child_sex.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model1_maternal_education.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model1_feeding_state.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model1_delivery_mode.csv")
 )
 
 ## Make a progress bar so I'm not driven mad by the expectation
@@ -35,23 +39,26 @@ res1 = ThreadsX.map(featlist) do feat
     next!(p)
     ab = vec(abundances(unstratified_unirefs_filtered[feat, :]))
     fit_one(indf, ab,
-        @formula(bug ~ mbiome_sample_age + child_sex),
+        @formula(bug ~ mbiome_sample_age + child_sex + maternal_education + feeding_state + delivery_mode),
         ProbitLink(),
-        ["mbiome_sample_age", "child_sex"];
+        ["mbiome_sample_age", "child_sex", "maternal_education", "feeding_state", "delivery_mode"];
         feature = string(feat))
 end
 
 ## res1 is Vector{Vector{NamedTuple}} (length 2 per feature). Split & write:
-write_results(outfiles1[1], getindex.(res1, 1))
-write_results(outfiles1[2], getindex.(res1, 2))
+for i in 1:length(outfiles1)
+    write_results(outfiles1[i], getindex.(res1, i))
+end
 
 # MODEL 2
-
 outfiles2 = (
-    "manuscript/FSEA/lms_Model2_mbiome_age.csv",
-    "manuscript/FSEA/lms_Model2_child_sex.csv",
-    "manuscript/FSEA/lms_Model2_visual.csv",
-    "manuscript/FSEA/lms_Model2_entropy.csv"
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_mbiome_age.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_child_sex.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_maternal_education.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_feeding_state.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_delivery_mode.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_visual.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model2_entropy.csv")
 )
 
 p = Progress(length(featlist))
@@ -59,25 +66,26 @@ res2 = ThreadsX.map(featlist) do feat
     next!(p)
     ab = vec(abundances(unstratified_unirefs_filtered[feat, :]))
     fit_one(indf, ab,
-        @formula(bug ~ mbiome_sample_age + child_sex + InfantVisAtt + MaternalEntropy),
+        @formula(bug ~ mbiome_sample_age + child_sex + maternal_education + feeding_state + delivery_mode + InfantVisAtt + MaternalEntropy),
         ProbitLink(),
-        ["mbiome_sample_age", "child_sex", "InfantVisAtt", "MaternalEntropy"];
+        ["mbiome_sample_age", "child_sex", "maternal_education", "feeding_state", "delivery_mode", "InfantVisAtt", "MaternalEntropy"];
         feature = string(feat))
 end
 
-write_results(outfiles2[1], getindex.(res2, 1))
-write_results(outfiles2[2], getindex.(res2, 2))
-write_results(outfiles2[3], getindex.(res2, 3))
-write_results(outfiles2[4], getindex.(res2, 4))
+for i in 1:length(outfiles2)
+    write_results(outfiles2[i], getindex.(res2, i))
+end
 
 # MODEL 3
-
 outfiles3 = (
-    "manuscript/FSEA/lms_Model3_mbiome_age.csv",
-    "manuscript/FSEA/lms_Model3_child_sex.csv",
-    "manuscript/FSEA/lms_Model3_visual.csv",
-    "manuscript/FSEA/lms_Model3_entropy.csv",
-    "manuscript/FSEA/lms_Model3_product.csv"
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_mbiome_age.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_child_sex.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_maternal_education.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_feeding_state.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_delivery_mode.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_visual.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_entropy.csv"),
+    joinpath(Base.pwd(),"manuscript", "gene_glm", "lms_Model3_product.csv")
 )
 
 p = Progress(length(featlist))
@@ -85,60 +93,12 @@ res3 = ThreadsX.map(featlist) do feat
     next!(p)
     ab = vec(abundances(unstratified_unirefs_filtered[feat, :]))
     fit_one(indf, ab,
-        @formula(bug ~ mbiome_sample_age + child_sex + InfantVisAtt + MaternalEntropy + MaternalEntropy & InfantVisAtt),
+        @formula(bug ~ mbiome_sample_age + child_sex + maternal_education + feeding_state + delivery_mode + InfantVisAtt + MaternalEntropy + MaternalEntropy & InfantVisAtt),
         ProbitLink(),
-        ["mbiome_sample_age", "child_sex", "InfantVisAtt", "MaternalEntropy", "MaternalEntropy & InfantVisAtt"];
+        ["mbiome_sample_age", "child_sex", "maternal_education", "feeding_state", "delivery_mode", "InfantVisAtt", "MaternalEntropy", "MaternalEntropy & InfantVisAtt"];
         feature = string(feat))
 end
 
-write_results(outfiles3[1], getindex.(res3, 1))
-write_results(outfiles3[2], getindex.(res3, 2))
-write_results(outfiles3[3], getindex.(res3, 3))
-write_results(outfiles3[4], getindex.(res3, 4))
-write_results(outfiles3[5], getindex.(res3, 5))
-
-# BONUS: MODEL 2 SUBTYPE 1 (ENTROPY ONLY)
-
-outfiles21 = (
-    "manuscript/FSEA/lms_Model21_mbiome_age.csv",
-    "manuscript/FSEA/lms_Model21_child_sex.csv",
-    "manuscript/FSEA/lms_Model21_entropy.csv"
-)
-
-p = Progress(length(featlist))
-res21 = ThreadsX.map(featlist) do feat
-    next!(p)
-    ab = vec(abundances(unstratified_unirefs_filtered[feat, :]))
-    fit_one(indf, ab,
-        @formula(bug ~ mbiome_sample_age + child_sex + MaternalEntropy),
-        ProbitLink(),
-        ["mbiome_sample_age", "child_sex", "MaternalEntropy"];
-        feature = string(feat))
+for i in 1:length(outfiles3)
+    write_results(outfiles3[i], getindex.(res3, i))
 end
-
-write_results(outfiles21[1], getindex.(res21, 1))
-write_results(outfiles21[2], getindex.(res21, 2))
-write_results(outfiles21[3], getindex.(res21, 3))
-
-# BONUS: MODEL 2 SUBTYPE 2 (VISUAL ONLY)
-
-outfiles22 = (
-    "manuscript/FSEA/lms_Model22_mbiome_age.csv",
-    "manuscript/FSEA/lms_Model22_child_sex.csv",
-    "manuscript/FSEA/lms_Model22_visual.csv"
-)
-
-p = Progress(length(featlist))
-res22 = ThreadsX.map(featlist) do feat
-    next!(p)
-    ab = vec(abundances(unstratified_unirefs_filtered[feat, :]))
-    fit_one(indf, ab,
-        @formula(bug ~ mbiome_sample_age + child_sex + InfantVisAtt),
-        ProbitLink(),
-        ["mbiome_sample_age", "child_sex", "InfantVisAtt"];
-        feature = string(feat))
-end
-
-write_results(outfiles22[1], getindex.(res22, 1))
-write_results(outfiles22[2], getindex.(res22, 2))
-write_results(outfiles22[3], getindex.(res22, 3))
